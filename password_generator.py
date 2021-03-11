d5df423f0d9352955879705d4a2992e065fe8e41
lunAr-creator <lunArcreator3@gmail.com>
2021-03-11 15:49:03 +0000

    updated password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -19,8 +19,34 @@ class password:
     def result(self, num) -> None:
         return password_result[num]
 
+class complex(password):
+    def __init__(self, length, string_method, numbers=True, special_chars=False):
+        characters = ''
+
+        methods: dict = {
+            "upper": string.ascii_uppercase,
+            "lower": string.ascii_lowercase,
+            "both": string.ascii_letters,
+        }
+
+        characters += methods[string_method]
+
+        if numbers:
+            characters += string.digits
+        if special_chars:
+            characters += string.punctuation
+
+        super().__init__(length=length, characters=characters)
+
 if __name__ == "__main__":
     var = password(20, 'abcdefghijklmnopqrstuvwxyz0123467589')
     print(var.generate(3))
 
     print(var.result(1))
+
+    # NOTE TO SELF: COMPLEX IS PRODUCING DOUBLE THE IERATION OF PASSWORDS. FIX THIS!!!!
+
+    var1 = complex(20, "both", True, True)
+    print(var1.generate(3))
+
+    print(var1.result(1))
