e40843289517a742be3e5c160b6818f82bca650b
lunAr-creator <lunArcreator3@gmail.com>
2021-03-12 13:30:37 +0000

    updated password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -43,12 +43,14 @@ class complex(simple):
 def hash(password):
     salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
     pwdhash = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'),salt, 100000)
+
     pwdhash = binascii.hexlify(pwdhash)
     return (salt + pwdhash).decode('ascii')
 
 def verify_hash(stored_password, provided_password):
     salt = stored_password[:64]
     stored_password = stored_password[64:]
+    
     pwdhash = hashlib.pbkdf2_hmac('sha512',provided_password.encode('utf-8'),salt.encode('ascii'),100000)
     pwdhash = binascii.hexlify(pwdhash).decode('ascii')
     return pwdhash == stored_password
