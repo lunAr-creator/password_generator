ae74361b623897f8d95e8472976ee7242a6573cc
lunAr-creator <lunArcreator3@gmail.com>
2021-03-14 21:42:28 +0000

    updated __init.py

--- pw_gen/__init__.py
+++ pw_gen/__init__.py
@@ -1,27 +1,35 @@
-import hashlib, binascii, os
+import random
 import secrets
 import string
+import urllib.request
 
 password_result = []
 
-class simple:
-    def __init__(self, length: int, characters: str) -> None:
+class Simple:
+    def __init__(self, length: int, characters = None) -> None:
         self.length = length
         self.characters = characters
 
-    def generate(self, iterations):
+    def generate(self, num_of_passwords: int):
+        characters = ''
+
+        if self.characters is None:
+            characters = string.ascii_letters + string.digits
+        if self.characters is not None:
+            characters = self.characters
+
         password_result.clear()
-        for p in range(iterations):
+        for p in range(num_of_passwords):
             password: str = ''
             for c in range(self.length):
-                password += secrets.choice(self.characters)
+                password += secrets.choice(characters)
             password_result.append(password)
         return password_result
 
     def result(self, num: int) -> None:
         return password_result[num]
 
-class complex(simple):
+class Complex(Simple):
     def __init__(self, length, string_method, numbers=True, special_chars=False):
         characters = ''
 
@@ -34,38 +42,45 @@ class complex(simple):
         characters += methods[string_method]
 
         if numbers:
-            characters += string.digit
+            characters += string.digits
         if special_chars:
             characters += string.punctuation
 
         super().__init__(length=length, characters=characters)
 
-def hash(password):
-    salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
-    pwdhash = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'),salt, 100000)
+class Memorable(Complex):
+    def __init__(self, numbers=True):
+        self.numbers = numbers
 
-    pwdhash = binascii.hexlify(pwdhash)
-    return (salt + pwdhash).decode('ascii')
+    def generate_memorable(self, num_of_passwords):
+        word_url = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}
+        req = urllib.request.Request(word_url, headers=headers)
+        response = response = urllib.request.urlopen(req)
+        long_txt = response.read().decode()
+        words = long_txt.splitlines()
 
-def verify_hash(stored_password, provided_password):
-    salt = stored_password[:64]
-    stored_password = stored_password[64:]
-
-    pwdhash = hashlib.pbkdf2_hmac('sha512',provided_password.encode('utf-8'),salt.encode('ascii'),100000)
-    pwdhash = binascii.hexlify(pwdhash).decode('ascii')
-    return pwdhash == stored_password
+        password_result.clear()
+        for i in range(num_of_passwords):
+            password = ''
+            two_words = ''
+            for i in range(2):
+                two_words += secrets.choice(words).title()
+            password = two_words
+            if self.numbers == True:
+                for i in range(random.randint(3, 4)):
+                    password += secrets.choice(string.digits)
+            password_result.append(password)
+        return password_result
 
 # Test scenarios
 
 if __name__ == "__main__":
-    var = simple(20, 'abcdefghijklmnopqrstuvwxyz0123467589')
+    var = Simple(20)
     print(var.generate(3))
 
     print(var.result(1))
 
-    stored_password = hash(var.result(1))
-    print(stored_password)
-
-    p = input('Password: ')
-    if verify_hash(stored_password, p) == True:
-        print('The password is correct')
+    var2 = Memorable()
+    print(var2.generate_memorable(3))
+    print(var2.result(1))
