a3f21e6dc535a37febe9479f55a3e2fb66e98b5a
lunAr-creator <lunArcreator3@gmail.com>
2021-03-15 14:30:29 +0000

    Updated __init__.py

--- pw_gen/__init__.py
+++ pw_gen/__init__.py
@@ -3,35 +3,53 @@ import secrets
 import string
 import urllib.request
 
-password_result = []
-
-class Simple:
-    def __init__(self, length: int, characters = None) -> None:
+class Simple():
+    def __init__(self, length: int, characters = None):
+        '''A simple password'''
         self.length = length
         self.characters = characters
+        self.output = []
 
     def generate(self, num_of_passwords: int):
+        '''
+        Generates a password depending on the num_of_passwords
+        and the arugments provided in the simple class
+        '''
         characters = ''
-
         if self.characters is None:
             characters = string.ascii_letters + string.digits
-        if self.characters is not None:
+        else:
             characters = self.characters
 
-        password_result.clear()
-        for p in range(num_of_passwords):
-            password: str = ''
+        for i in range(num_of_passwords):
+            password = ''
             for c in range(self.length):
                 password += secrets.choice(characters)
-            password_result.append(password)
-        return password_result
+            self.output.append(password)
+        return self.output
 
-    def result(self, num: int) -> None:
-        return password_result[num]
+    def return_result(self, index: int):
+        '''
+        Returns the password which is at the specified index
+        in the output list.
+        '''
+        try:
+            return self.output[index]
+        except IndexError:
+            print(f'Incorrect index specified. Please provide an index relevant to the number of passwords generated')
+
+    def clear_results(self):
+        '''Clears the output list if you want to make way for new passwords'''
+        self.output.clear()
 
 class Complex(Simple):
     def __init__(self, length, string_method, numbers=True, special_chars=False):
+        '''
+        Creates a customisable password depending on length,
+        string_method, numbers and special_chars
+        '''
         characters = ''
+        self.output = []
 
         methods: dict = {
             "upper": string.ascii_uppercase,
@@ -48,11 +66,14 @@ class Complex(Simple):
 
         super().__init__(length=length, characters=characters)
 
-class Memorable(Complex):
+class Memorable(Simple):
     def __init__(self, numbers=True):
+        '''A memorable password'''
         self.numbers = numbers
+        self.output = []
 
-    def generate_memorable(self, num_of_passwords):
+    def generate(self, num_of_passwords: int):
+        '''Gets some random words'''
         word_url = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}
         req = urllib.request.Request(word_url, headers=headers)
@@ -60,7 +81,10 @@ class Memorable(Complex):
         long_txt = response.read().decode()
         words = long_txt.splitlines()
 
-        password_result.clear()
+        '''
+        Generates the password containing 2 words
+        and numbers if self.numbers == True
+        '''
         for i in range(num_of_passwords):
             password = ''
             two_words = ''
@@ -70,5 +94,11 @@ class Memorable(Complex):
             if self.numbers == True:
                 for i in range(random.randint(3, 4)):
                     password += secrets.choice(string.digits)
-            password_result.append(password)
-        return password_result
+            self.output.append(password)
+        return self.output
+
+# Test Scenarios
+
+if __name__ == "__main__":
+    var = Memorable()
+    print(var.generate(3))
